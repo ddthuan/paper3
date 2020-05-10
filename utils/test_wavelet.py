5ea5a696933b4872acec63206c00b006a8a24977
ddthuan <59833376+ddthuan@users.noreply.github.com>
2020-05-10 13:16:08 +0800

    Update test_wavelet.py

--- utils/test_wavelet.py
+++ utils/test_wavelet.py
@@ -26,6 +26,22 @@ class CConv(nn.Module):
         self.conv = nn.Conv2d(1,4, kernel_size=kernel_size, stride=(2,2), padding=(1,1), bias=False)
     
     def forward(self, x):
+      
+      
+w1=pywt.ContinuousWavelet('cmor1.5-1.0')
+lp_c = w1.wavefun()[1]
+real_c = np.real(w1.wavefun()[0])
+imag_c = np.imag(w1.wavefun()[0])
+
+plt.plot(lp_c)
+plt.show()
+
+plt.plot(real_c)
+plt.show()
+
+plt.plot(imag_c)
+plt.show()
+
         
         filters = torch.stack([
                 self.real[None] * self.real[:,None],
